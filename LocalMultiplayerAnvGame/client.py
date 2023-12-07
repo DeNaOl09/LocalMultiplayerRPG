be440d7f4d261f7f9b2300e78e3b60cfd499db4d
DeNaOl09 <nazar.delmukhametov@mail.ru>
2023-12-07 17:41:05 +0500

    Added button class, now you're able to walk

--- LocalMultiplayerAnvGame/client.py
+++ LocalMultiplayerAnvGame/client.py
@@ -12,10 +12,23 @@ nick = input('Введите ник >>> ')
 playerclass = input('Выберите класс: 1-варвар: ')
 
 
-
 class Button:
     def __init__(self, x, y, pic):
-        pass
+        self.img = pic
+        self.rect = self.img.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        window.blit(self.img, (self.rect.x, self.rect.y))
+
+        if pg.mouse.get_pressed()[0]:
+            mx, my = pg.mouse.get_pos()
+            if self.rect.x <= mx <= self.rect.x+self.rect.width:
+                if self.rect.y <= my <= self.rect.y+self.rect.height:
+                    return True
+
+        return False
 
 
 class Cell:
@@ -46,6 +59,12 @@ dmg = 0
 x = 100
 y = 100
 
+pg.font.init()
+mfont = pg.font.SysFont('Arial', 24)
+
+buttons = []
+walk = Button(1000, 0, pg.image.load('images/go.png'))
+
 if playerclass == '1':
     hp = 100
     dmg = 5
@@ -71,6 +90,8 @@ clock = pg.time.Clock()
 running = True
 selected = (-1, -1)
 clickcd = time()
+info_text = mfont.render('', True, (0, 255, 255))
+infocd = time()
 
 while running:
     clock.tick(60)
@@ -89,6 +110,16 @@ while running:
         info = cell.split(';')
         if len(info) == 4:
             cells.append(Cell(info[0], info[1], info[2], info[3]))
+
+    if walk.update():
+        if (selected[0] - x)**2 + (selected[1] - y)**2 <= 200**2:
+            x, y = selected[0], selected[1]
+        else:
+            info_text = mfont.render('Слишком далеко!', True, (0, 255, 255))
+            infocd = time()
+
+    if time()-infocd <= 5:
+        window.blit(info_text, (1000, 500))
 
     '''Обработка событий'''
     for event in pg.event.get():
